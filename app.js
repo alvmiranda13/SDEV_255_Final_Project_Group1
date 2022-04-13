da8b9a1e2000978661819c8f7f7fb522b6e75a92
alvmiranda13 <amiranda18@ivytech.edu>
2022-04-13 16:40:08 -0400

    Alot of changes added, Bootstrap Implemented

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
-const app = express();;
+const path = require('path');
+const app = express();
+/// init bootstrap
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
+
 /// view engine
 app.set('view engine', 'ejs');
 //port set
 app.listen(3000);
 //navigation routing
 
-//render index page
+//render pages//
 app.get('/', (req,res)=>{
     res.render('index', {title: 'home'});
 });
-//
+app.get('/courses', (req,res)=>{
+    res.render('courses', {title: 'courses'});
+});
+app.get('/login', (req,res)=>{
+    res.render('login', {title: 'login'});
+});
+
+
+
+
+
+//error 404//
+app.use((req,res)=>{
+    res.status(404).render('404', { title: '404' });
+});
