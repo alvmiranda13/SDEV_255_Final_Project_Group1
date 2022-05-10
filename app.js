84886dcb2b8dee7b1c0b2aca99d113f031ccb65a
alvmiranda13 <amiranda18@ivytech.edu>
2022-05-10 03:09:35 -0400

    Added MONGODB and Courses MODEL courses has been changed to viewcourses, etc.

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Courses = require('./models/courses');
 const path = require('path');
 const app = express();
+const { render } = require('express/lib/response');
+
+/// mongoose db connect
+const dbURI = 'mongodb+srv://user:passwordUser123@finalprojectdb.gffp0.mongodb.net/finalProjectDB?retryWrites=true&w=majority';
+mongoose.connect(dbURI);
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result)=> app.listen(3000))
+    //keep this to makesure we are connected to db//
+    .then((result)=> console.log('connected to db'))
+    .catch((err) => console.log(err));
+
 /// init bootstrap
 app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
 /// view engine
 app.set('view engine', 'ejs');
-//port set
-app.listen(3000);
 //navigation routing
 
 //render pages//
@@ -18,11 +29,24 @@ app.get('/', (req,res)=>{
 app.get('/login', (req,res)=>{
     res.render('login', {title: 'login'});
 });
+
+////courses routes////
 app.get('/courseDescription', (req,res)=>{
+    //individual course descriptions
     res.render('courseDescription', {title: 'courseDescription'});
+    
 });
-app.get('/courses', (req,res)=>{
-    res.render('courses', {title: 'courses'});
+app.get('/viewcourses', (req,res)=>{
+   //display all courses
+    
+    Courses.find()
+    .then((result)=>{
+        res.render('viewcourses', {title: 'Courses', Courses: result});        
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+    
 });
 app.get('/registration', (req,res)=>{
     res.render('registration', {title: 'registration'});
